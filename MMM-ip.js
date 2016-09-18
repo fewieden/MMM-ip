301c5ad9a8ec204677e0bec999eeeb6d255c4727
fewieden <fewieden@htwg-konstanz.de>
2016-09-18 12:02:45 +0200

    display wlan and ethernet

--- MMM-ip.js
+++ MMM-ip.js
@@ -1,30 +1,50 @@
 Module.register("MMM-ip",{
+
+    ips: {},
+
+    types: ["eth0", "wlan0"],
+
     defaults: {
         fontSize: 9,
         dimmed: true,
-        show: 'both'
+        showFamily: 'both',
+        showType: 'both'
     },
 
     start: function() {
         Log.info("Starting module: " + this.name);
-        this.ip = {};
-        this.sendSocketNotification('GET_IP', {});
+        this.sendSocketNotification('GET_IPS', {});
+    },
+
+    getStyles: function () {
+        return ["MMM-ip.css"];
     },
 
     getDom: function() {
         var wrapper = document.createElement("div");
-        var keys = Object.keys(this.ip);
-        if(keys.length > 0){
+        var typeKeys = Object.keys(this.ips);
+        if(typeKeys.length > 0){
             if(this.config.dimmed){
                 wrapper.classList.add('dimmed');
             }
             wrapper.style.fontSize = this.config.fontSize + 'px';
-            for(var i = 0; i < keys.length; i++){
-                if(this.config.show === 'both' || this.config.show === keys[i]){
-                    var div = document.createElement("div");
-                    div.style.lineHeight = '1.2em';
-                    div.innerHTML = keys[i] + ': ' + this.ip[keys[i]];
-                    wrapper.appendChild(div);
+            for(var i = 0; i < typeKeys.length; i++){
+                if((this.config.showType === 'both' || this.config.showType === typeKeys[i]) && this.types.indexOf(typeKeys[i]) !== -1){
+                    var familyKeys = Object.keys(this.ips[typeKeys[i]]);
+                    if(familyKeys.length > 0){
+                        if(this.config.dimmed){
+                            wrapper.classList.add('dimmed');
+                        }
+                        wrapper.style.fontSize = this.config.fontSize + 'px';
+                        for(var n = 0; n < familyKeys.length; n++){
+                            if(this.config.showFamily === 'both' || this.config.showFamily === this.ips[typeKeys[i][familyKeys[n]]].family){
+                                var div = document.createElement("div");
+                                div.classList.add("line");
+                                div.innerHTML = typeKeys[i].toUpperCase() + " " + this.ips[typeKeys[i][familyKeys[n]]].family + ": " + this.ips[typeKeys[i][familyKeys[n]]].address;
+                                wrapper.appendChild(div);
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -32,8 +52,8 @@ Module.register("MMM-ip",{
     },
 
     socketNotificationReceived: function(notification, payload) {
-        if (notification === "IP"){
-            this.ip[payload.type] = payload.ip;
+        if (notification === "IPS"){
+            this.ips = payload;
             this.updateDom();
         }
     }
--- node_helper.js
+++ node_helper.js
@@ -3,17 +3,8 @@ const interfaces = require('os').networkInterfaces();
 
 module.exports = NodeHelper.create({
     socketNotificationReceived: function(notification, payload) {
-        if (notification === 'GET_IP') {
-            for(interface in interfaces){
-                for(networkaddress in interfaces[interface]){
-                    if((interfaces[interface][networkaddress].family === 'IPv4' || interfaces[interface][networkaddress].family === 'IPv6') && !interfaces[interface][networkaddress].internal) {
-                        this.sendSocketNotification('IP', {
-                            'type': interfaces[interface][networkaddress].family,
-                            'ip': interfaces[interface][networkaddress].address
-                        });
-                    }
-                }
-            }
+        if (notification === 'GET_IPS') {
+            this.sendSocketNotification('IPS', interfaces);
         }
     }
 });
