33ed00554f44ff8d2bfb54a7856f0de493e3578c
fewieden <fewieden@htwg-konstanz.de>
2016-10-04 16:31:27 +0200

    added voice control

--- MMM-ip.js
+++ MMM-ip.js
@@ -1,11 +1,40 @@
+/* Magic Mirror
+ * Module: MMM-ip
+ *
+ * By fewieden https://github.com/fewieden/MMM-ip
+ * MIT Licensed.
+ */
+
 Module.register("MMM-ip",{
 
     ips: {},
 
     types: ["eth0", "wlan0"],
 
+    interfaces: false,
+
+    help: false,
+
+    voice: {
+        mode: "NETWORK",
+        sentences: [
+            "OPEN HELP",
+            "CLOSE HELP",
+            "SHOW INTERFACES",
+            "HIDE INTERFACES"
+        ]
+    },
+
+    getTranslations: function () {
+        return {
+            en: "translations/en.json",
+            de: "translations/de.json"
+        };
+    },
+
     defaults: {
         fontSize: 9,
+	    voice: false,
         dimmed: true,
         showFamily: 'both',
         showType: 'both'
@@ -20,33 +49,73 @@ Module.register("MMM-ip",{
         return ["MMM-ip.css"];
     },
 
+    notificationReceived: function (notification, payload, sender) {
+        if(notification === "ALL_MODULES_STARTED"){
+            this.sendNotification("REGISTER_VOICE_MODULE", this.voice);
+        } else if(notification === "VOICE_INTERFACES" && sender.name === "MMM-voice"){
+            this.checkCommands(payload);
+        } else if(notification === "VOICE_MODE_CHANGED" && sender.name === "MMM-voice" && payload.old === this.voice.mode){
+            this.help = false;
+            this.interfaces = false;
+            this.updateDom(300);
+        }
+    },
+
+    checkCommands: function(data){
+        if(/(HELP)/g.test(data)){
+            if(/(CLOSE)/g.test(data) || this.help && !/(OPEN)/g.test(data)){
+                this.help = false;
+            } else if(/(OPEN)/g.test(data) || !this.help && !/(CLOSE)/g.test(data)){
+                this.interfaces = false;
+                this.help = true;
+            }
+        } else if(/(INTERFACES)/g.test(data)){
+            if(/(HIDE)/g.test(data) || this.help && !/(SHOW)/g.test(data)){
+                this.interfaces = false;
+            } else if(/(SHOW)/g.test(data) || !this.help && !/(HIDE)/g.test(data)){
+                this.help = false;
+                this.interfaces = true;
+            }
+        }
+        this.updateDom(300);
+    },
+
+
+
     getDom: function() {
         var wrapper = document.createElement("div");
+        if(this.config.voice){
+            document.getElementById(this.identifier).classList.add("voice-mode");
+        } else {
+            wrapper.style.fontSize = this.config.fontSize + 'px';
+        }
         var typeKeys = Object.keys(this.ips);
         if(typeKeys.length > 0){
             if(this.config.dimmed){
                 wrapper.classList.add('dimmed');
             }
-            wrapper.style.fontSize = this.config.fontSize + 'px';
-            for(var i = 0; i < typeKeys.length; i++){
-                if((this.config.showType === 'both' || this.config.showType === typeKeys[i]) && this.types.indexOf(typeKeys[i]) !== -1){
-                    var familyKeys = Object.keys(this.ips[typeKeys[i]]);
-                    if(familyKeys.length > 0){
-                        if(this.config.dimmed){
-                            wrapper.classList.add('dimmed');
-                        }
-                        wrapper.style.fontSize = this.config.fontSize + 'px';
-                        for(var n = 0; n < familyKeys.length; n++){
-                            if(this.config.showFamily === 'both' || this.config.showFamily === this.ips[typeKeys[i]][familyKeys[n]].family){
-                                var div = document.createElement("div");
-                                div.classList.add("line");
-                                div.innerHTML = typeKeys[i].toUpperCase() + " " + this.ips[typeKeys[i]][familyKeys[n]].family + ": " + this.ips[typeKeys[i]][familyKeys[n]].address;
-                                wrapper.appendChild(div);
-                            }
-                        }
+            wrapper.classList.add("modal", "align-left");
+
+            var modules = document.querySelectorAll(".module");
+            for (var i = 0; i < modules.length; i++) {
+                if(!modules[i].classList.contains("MMM-ip")){
+                    if(this.interfaces || this.help){
+                        modules[i].classList.add("MMM-ip-blur");
+                    } else {
+                        modules[i].classList.remove("MMM-ip-blur");
                     }
                 }
             }
+
+            if(this.interfaces){
+                this.appendInterfaces(typeKeys, wrapper);
+            } else if(this.help) {
+                this.appendHelp(wrapper);
+            }
+        } else {
+            var text = document.createElement("div");
+            text.innerHTML = this.translate("LOADING");
+            wrapper.appendChild(text);
         }
         return wrapper;
     },
@@ -56,5 +125,67 @@ Module.register("MMM-ip",{
             this.ips = payload;
             this.updateDom();
         }
+    },
+
+    appendInterfaces: function(typeKeys, appendTo){
+        var header = document.createElement("div");
+        header.classList.add("bright");
+        header.innerHTML = this.translate("NETWORK_INTERFACES");
+        appendTo.appendChild(header);
+        for(var i = 0; i < typeKeys.length; i++){
+            if((this.config.showType === 'both' || this.config.showType === typeKeys[i]) && this.types.indexOf(typeKeys[i]) !== -1){
+                var familyKeys = Object.keys(this.ips[typeKeys[i]]);
+                if(familyKeys.length > 0){
+                    if(this.config.voice){
+                        var type = document.createElement("div");
+                        type.classList.add("thin");
+                        type.innerHTML = typeKeys[i].toUpperCase();
+                        appendTo.appendChild(type);
+                        var mac_address = null;
+                    }
+                    for(var n = 0; n < familyKeys.length; n++){
+                        if(this.config.showFamily === 'both' || this.config.showFamily === this.ips[typeKeys[i]][familyKeys[n]].family){
+                            var ip = document.createElement("div");
+                            if(this.config.voice){
+                                mac_address = this.ips[typeKeys[i]][familyKeys[n]].mac;
+                                ip.innerHTML = this.ips[typeKeys[i]][familyKeys[n]].family + ": " + this.ips[typeKeys[i]][familyKeys[n]].address;
+                            } else {
+                                ip.classList.add("line");
+                                ip.innerHTML = typeKeys[i].toUpperCase() + " " + this.ips[typeKeys[i]][familyKeys[n]].family + ": " + this.ips[typeKeys[i]][familyKeys[n]].address;
+                            }
+                            appendTo.appendChild(ip);
+                        }
+                    }
+                    if(this.config.voice && mac_address){
+                        var mac = document.createElement("div");
+                        mac.innerHTML = "MAC: " + mac_address;
+                        appendTo.appendChild(mac);
+                    }
+                }
+            }
+        }
+    },
+
+    appendHelp: function(appendTo){
+        var title = document.createElement("h1");
+        title.classList.add("medium");
+        title.innerHTML = this.name + " - " + this.translate("COMMAND_LIST");
+        appendTo.appendChild(title);
+
+        var mode = document.createElement("div");
+        mode.innerHTML = this.translate("MODE") + ": " + this.voice.mode;
+        appendTo.appendChild(mode);
+
+        var listLabel = document.createElement("div");
+        listLabel.innerHTML = this.translate("VOICE_COMMANDS") + ":";
+        appendTo.appendChild(listLabel);
+
+        var list = document.createElement("ul");
+        for(var i = 0; i < this.voice.sentences.length; i++){
+            var item = document.createElement("li");
+            item.innerHTML = this.voice.sentences[i];
+            list.appendChild(item);
+        }
+        appendTo.appendChild(list);
     }
 });
--- README.md
+++ README.md
@@ -3,31 +3,19 @@ IPv4/IPv6 Network Address Module for MagicMirror<sup>2</sup>
 
 ## Dependencies
   * An installation of [MagicMirror<sup>2</sup>](https://github.com/MichMich/MagicMirror)
+  * OPTIONAL: [Voice Control](https://github.com/fewieden/MMM-voice)
   * Node.js > v0.6.0 for os.networkInterfaces()
 
 ## Installation
  1. Clone this repo into `~/MagicMirror/modules` directory.
  2. Configure your `~/MagicMirror/config/config.js`:
 
-  * Basic Config:
-
-    ```
-    {
-        module: 'MMM-ip',
-        position: 'bottom_right'
-    }
-    ```
-  * Full Config:
-
     ```
     {
         module: 'MMM-ip',
         position: 'bottom_right',
         config: {
-            fontSize: 12,
-            dimmed: false
-            showFamily: 'IPv4',
-            showType: 'eth0'
+            ...
         }
     }
     ```
@@ -35,7 +23,20 @@ IPv4/IPv6 Network Address Module for MagicMirror<sup>2</sup>
 ## Config Options
 | **Option** | **Default** | **Description** |
 | --- | --- | --- |
-| `fontSize` | 9 | Font size in pixels |
-| `dimmed` | true | Boolean for discrete visibility |
-| `showFamily` | 'both' | Network Address family to display 'IPv4', 'IPv6' or 'both' |
-| `showType` | 'both' | Network interface type to display 'eth0', 'wlan0' or 'both' |
+| `fontSize` | `9` | Font size in pixels. Only if `voice` is set to `false` |
+| `dimmed` | `true` | Boolean for discrete visibility |
+| `showFamily` | `'both'` | Network Address family to display `'IPv4'`, `'IPv6'` or `'both'` |
+| `showType` | `'both'` | Network interface type to display `'eth0'`, `'wlan0'` or `'both'` |
+| `voice` | `false` | Boolean for optional voice commands |
+
+## OPTIONAL: Voice Control
+This module supports voice control by [MMM-voice](https://github.com/fewieden/MMM-voice). In order to use this feature, it's required to install the voice module and set voice in config options to true.
+
+### Mode
+The voice control mode for this module is `NETWORK`
+
+### List of all Voice Commands
+  * OPEN HELP -> Shows the information from the readme here with mode and all commands.
+  * CLOSE HELP -> Hides the help information.
+  * SHOW INTERFACES -> Shows network interfaces based on config options with mac addresses.
+  * HIDE INTERFACES -> Hide network interfaces.
